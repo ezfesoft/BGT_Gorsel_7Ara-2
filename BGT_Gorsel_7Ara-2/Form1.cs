a3f8198e1b940ae4d4eebd93c51b5d2a177e4ae4
Lab1-1 <Lab1-1@DESKTOP-NHSN8JG>
2023-12-07 10:56:17 +0300

    Proje dosyası ekle.

---
+++ BGT_Gorsel_7Ara-2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_Gorsel_7Ara_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double not1;
+            double not2;
+            double sonuc;
+
+            not1 = Convert.ToDouble(textBox1.Text);
+            not2 = Convert.ToDouble(textBox2.Text);
+
+            sonuc = (not1 * 0.4) + (not2 * 0.6);
+
+            label4.Text = sonuc.ToString();
+
+            if (sonuc>=50)
+            {
+                label6.Text = "BAŞARILI";
+            }
+            if (sonuc < 50)
+            {
+                label6.Text = "BAŞARISIZ";
+            }
+        }
+    }
+}
